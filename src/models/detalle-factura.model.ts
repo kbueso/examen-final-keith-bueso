36f398fc9f5a20c712d8af9f045d1735dfbd904c
Keith Bennett <86028884+kbueso@users.noreply.github.com>
2021-12-20 21:33:54 -0600

    agregando datasource y modelos

---
+++ src/models/detalle-factura.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class DetalleFactura extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  facturaId: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  productoId: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<DetalleFactura>) {
+    super(data);
+  }
+}
+
+export interface DetalleFacturaRelations {
+  // describe navigational properties here
+}
+
+export type DetalleFacturaWithRelations = DetalleFactura & DetalleFacturaRelations;
